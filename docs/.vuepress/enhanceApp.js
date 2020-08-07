49fd93f4af1a9b9ffe1562521bf93b0d155b5bbc
fanye <1518878598@qq.com>
2020-08-07 22:45:37 +0800

    feat:  enhanceApp

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,8 +1,8 @@
 export default ({
                     Vue, // the version of Vue being used in the VuePress app
-                    options, // the options for the root Vue instance
-                    router, // the router instance for the app
-                    siteData // site metadata
+                    // options, // the options for the root Vue instance
+                    // router, // the router instance for the app
+                    // siteData // site metadata
                 }) => {
     // TODO: Input中使用了window对象，在node编译过程中无法使用，因此存在报错。
     // TODO: 这里使用异步组件绕过了验证。
